fda17819344f49c0aa0224ca2b8b41ada72c8996
p0d4r14n <p0d4r14n@protonmail.com>
2022-05-28 14:35:03 +0200

    solution b

--- 2015/day04/solution_b.zig
+++ 2015/day04/solution_b.zig
@@ -1,19 +1,20 @@
 const std = @import("std");
 const input = @embedFile("./input.txt");
 const expect = std.testing.expect;
+const Md5 = std.crypto.hash.Md5;
 
-fn findNumber(comptime length: usize, content: *const [length]u8) i32 {
-    return std.crypto.hash.Md5.hash(content);  
+fn findNumber(comptime length: usize, content: *const [length]u8) !usize {
+    var out: [16]u8 = undefined;
+    var i: usize = 0;
+    while (true) : (i += 1) {
+        const hash_new: []u8 = try std.fmt.bufPrint(&out, "{s}{}", .{content, i});
+        Md5.hash(hash_new, &out, .{});
+        if (out[0] == 0 and out[1] == 0 and out[2] == 0) {
+            return i;
+        }
+    }
 }
 
 pub fn main() void {
-    std.log.debug("Feet: {d}", .{findNumber(input.len, input)});
-}
-
-test "A present with dimensions `2x3x4` requires a total of `34` feet" {
-    try expect(calculateFeet(2, 3, 4) == 34);
-}
-
-test "A present with dimensions `1x1x10` requires a total of `14` feet" {
-    try expect(calculateFeet(1, 1, 10) == 14);
+    std.log.debug("Number: {d}", .{findNumber(input.len, input)});
 }
